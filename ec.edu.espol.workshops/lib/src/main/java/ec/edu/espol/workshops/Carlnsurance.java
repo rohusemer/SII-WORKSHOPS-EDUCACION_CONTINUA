978cbf3ad170716cb880b3e45fe2d58e9490c99e
rohusemer <ronhuseg@espol.edu.ec>
2021-10-18 15:39:19 -0500

    Se añadio la constante primabase

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Carlnsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Carlnsurance.java
@@ -4,12 +4,14 @@ public class Carlnsurance {
 	private int edad;
 	private char[] sexo = {"M", "F"};
 	private String[] eCivil = {"Casado", "No Casado"};
+	static final int PRIMABASE = 500;
 
 	public Carlnsurance(String edad, char[] sexo, String[] eCivil) {
 		this.edad = edad;
 		this.sexo = sexo;
 		this.eCivil = eCivil;
 	}
+	
 	
 }
 
