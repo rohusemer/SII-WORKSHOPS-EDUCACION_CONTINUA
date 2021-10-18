58efabbb21e881a49260b3b7a6834fc6d74ef7cd
rohusemer <ronhuseg@espol.edu.ec>
2021-10-18 15:32:17 -0500

    Declaracion de atributos en la clase Carlnsurance

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Carlnsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Carlnsurance.java
@@ -1,5 +1,15 @@
 package ec.edu.espol.workshops;
 
 public class Carlnsurance {
+	private int edad;
+	private char[] sexo = {"M", "F"};
+	private String[] eCivil = {"Casado", "No Casado"};
 
+	public Carlnsurance(String edad, char[] sexo, String[] eCivil) {
+		this.edad = edad;
+		this.sexo = sexo;
+		this.eCivil = eCivil;
+	}
+	
 }
+
