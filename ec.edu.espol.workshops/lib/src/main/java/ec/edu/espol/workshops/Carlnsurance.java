a8927fdef8feb0c732b0090f10aeb3463db30c3e
densreye <densreye@espol.edu.ec>
2021-10-18 19:06:24 -0500

    Getters y setters añadidos

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Carlnsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Carlnsurance.java
@@ -6,12 +6,40 @@ public class Carlnsurance {
 	private String[] eCivil = {"Casado", "No Casado"};
 	static final int PRIMABASE = 500;
 
-	public Carlnsurance(String edad, char[] sexo, String[] eCivil) {
+	public Carlnsurance(int edad, char[] sexo, String[] eCivil) {
 		this.edad = edad;
 		this.sexo = sexo;
 		this.eCivil = eCivil;
 	}
 	
+	public void setEdad(int edad) {
+		
+		this.edad = edad;
+		
+	}
 	
+	public String getEdad() {
+		return this.edad
+	}
+	
+	public void setSexo(char sexo) {
+		
+		this.sexo = sexo;
+		
+	}
+	
+	public String getSexo() {
+		return this.sexo
+	}
+	
+	public void setEstado(int estado) {
+		
+		this.estado = estado;
+		
+	}
+	
+	public String getEstado() {
+		return this.estado 
+	}
 }
 
